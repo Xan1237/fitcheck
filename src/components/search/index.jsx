cde40c727c0b837dfd4ddd0ccffde3972bd0ae71
Xan1237 <xn543440@dal.ca>
2025-02-11 17:05:59 -0400

    Added Search Bar Compnent

---
+++ src/components/search/index.jsx
@@ -0,0 +1,14 @@
+import "./style.scss"
+
+
+
+function search() {
+  return (
+    <>
+            <input class="search-bar-container" type="text"></input>
+            <button class="btn">Search</button>
+    </>
+  )
+}
+
+export default search

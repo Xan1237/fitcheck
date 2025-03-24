f62880f2c86fa0618c68c14f1ba553ec6e7c558b
Saul Hafting <saulhafting@gmail.com>
2025-03-24 10:40:50 -0300

    fixing merge error

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,25 +1,15 @@
 import express, { json } from "express";
-import {
-  createComment,
-  getAdress,
-  getComments,
-  profile,
-  userInfo,
-  getGymData,
-} from "../controllers/userController.js"; // Updated import
-import { signUpUser } from "../middlewares/auth_signup_password.js";
-import { signInUser } from "../middlewares/auth_signin_password.js";
-
+import { createComment, getAdress, getComments, profile, userInfo, getUserName} from "../controllers/userController.js"; // Correct import
+import {signUpUser} from '../middlewares/auth_signup_password.js'
+import {signInUser} from '../middlewares/auth_signin_password.js'
 const router = express.Router();
-
-// Define the routes
+// Define the route for creating a comment
 router.post("/api/comment", createComment);
-router.post("/api/address", getAdress);
-router.get("/api/GetComments", getComments);
-router.post("/auth/signup", signUpUser);
-router.post("/auth/signin", signInUser);
-router.post("/api/profile", profile);
-router.get("/api/GetUserData", userInfo);
-router.get("/api/getGymData", getGymData); // Endpoint for getting gym data with tags
-
+router.post("/api/address", getAdress)
+router.get("/api/GetComments", getComments)
+router.post("/auth/signup", signUpUser)
+router.post("/auth/signin", signInUser)
+router.post("/api/profile", profile)
+router.get("/api/GetUserData", userInfo)
+router.post("/api/getUserName", getUserName)
 export default router;

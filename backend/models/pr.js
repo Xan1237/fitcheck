43e63ba2d1e069952d737cf2e15637621b430f1a
Xan1237 <154279844+Xan1237@users.noreply.github.com>
2025-05-25 22:41:26 -0300

    added lots of fit for less locations

---
+++ backend/models/pr.js
@@ -0,0 +1,9 @@
+class PR {
+    constructor(username, exercise_name, weight, reps) {
+        this.username = username;
+        this.exercise_name = exercise_name;
+        this.weight = weight;
+        this.reps = reps;
+    }
+}
+export default PR;

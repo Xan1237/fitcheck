412bae0e9a5169223cf874838d266fc87c56048d
Xan1237 <154279844+Xan1237@users.noreply.github.com>
2025-07-24 16:47:56 -0300

    removed feed redirect

--- frontend/src/pages/userAuth.jsx
+++ frontend/src/pages/userAuth.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const REDIRECT_URL =  '/feed'; // <-- Change to home page
+const REDIRECT_URL =  '/'; // <-- Change to home page
 
 const AuthPage = () => {
   const [email, setEmail] = useState('');
